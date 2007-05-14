d61f0cdee4cf86d155c33678ded1c74fe5ec02ec
William Lipscomb <lipscomb@ucar.edu>
2007-05-14 17:37:28 +0000

    Consistent changes in Init and Run modules of all drivers. (In the last round, I made these changes only in the cice4 versions.)
    (1) Replaced init_remap subroutine with init_transport.
    
    (2) Removed istop, jstop, and lstop from the call list for aggregate_area.
        This means that aggregate_area can compute an ice area > 1.0
        without terminating the model.  For certain tests it is useful
        to allow aice > 1.0.  For most runs, however, aice > 1.0 should
        be limited to 1.0, else the code should abort.  
        The user can now choose not to abort by setting l_limit_aice = F 
        when calling cleanup_itd.  By default, l_limit_aice = T.

--- cice/drivers/ccsm_concurrent/CICE_RunMod.F90
+++ cice/drivers/ccsm_concurrent/CICE_RunMod.F90
@@ -795,11 +795,9 @@
       !-----------------------------------------------------------------
       ! Compute fractional ice area in each grid cell.
       !-----------------------------------------------------------------
-            call aggregate_area (nx_block, ny_block, &
-                           aicen(:,:,:,iblk), &
-                           aice(:,:,iblk),     aice0(:,:,iblk),  &
-                           l_stop, &
-                           istop,    jstop)
+            call aggregate_area (nx_block,          ny_block, &
+                                 aicen(:,:,:,iblk),           &
+                                 aice (:,:,  iblk), aice0(:,:,iblk))
 
       !-----------------------------------------------------------------
       ! Identify grid cells with ice.
