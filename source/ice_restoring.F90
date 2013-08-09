0b9242cf8fe03d51484b69b9dbebd9fd4a11deb5
Elizabeth Hunke <eclare@lanl.gov>
2013-08-09 21:46:39 +0000

    Force Neumann boundary conditions on regional grids with open boundaries.
    The following namelist settings are required for runs on regional grids
    with open boundary conditions:
    
      , restart_ext    = .true.
      , ew_boundary_type  = 'open'
      , ns_boundary_type  = 'open'
      , restore_ice     = .true.
    
    In addition, two rows/columns of cells around the regional grid edge
    (one halo, one physical) must be defined for the arrays to which the
    state variables are restored, in ice_HaloRestore_init.   The changes 
    in this revision force Neumann boundary conditions along those edges, 
    but only to the values defined by the initial ice state.  Its up to 
    the user to define these boundaries differently, if desired.
    
    Note that grid_type can be either rectangular or displaced_pole.  
    Perhaps we should add a regional option with its own subroutine?
    
    Additional notes from the r499 log:
    
    Add routines for gathering, scattering and (unformatted) reading and writing 
    of the "extended" global grid, including the physical domain and ghost cells 
    around the outer edges. These modifications allow exact restarts on regional 
    grids with open boundary conditions, and they will also simplify restarts on 
    the various tripole grids. No netcdf changes yet.
    
    Contrary to what the cicedoc says, Neumann conditions are not available for 
    open boundaries---the current infrastructure is not flexible enough to allow 
    both local copies and messages along the same edge, which would be necessary 
    for Neumann conditions in corners. Instead, restoring is required to enforce 
    open boundary conditions (as in POP).

--- source/ice_restoring.F90
+++ source/ice_restoring.F90
@@ -68,10 +68,21 @@
 
 ! !USES:
 
+      use ice_blocks, only: block, get_block, nblocks_x, nblocks_y
       use ice_communicate, only: my_task, master_task
-      use ice_domain, only: ew_boundary_type, ns_boundary_type
+      use ice_domain, only: ew_boundary_type, ns_boundary_type, &
+          nblocks, blocks_ice
       use ice_fileunits, only: nu_diag
       use ice_restart, only: restart_ext
+
+   integer (int_kind) :: &
+     i,j,iblk,nt,n,      &! dummy loop indices
+     ilo,ihi,jlo,jhi,    &! beginning and end of physical domain
+     ibc,                &! ghost cell column or row
+     npad                 ! padding column/row counter
+
+   type (block) :: &
+     this_block  ! block info for current block
 
    if (.not. restore_ice) return
 
@@ -87,8 +98,11 @@
              vsnon_rest(nx_block,ny_block,ncat,max_blocks), &
              trcrn_rest(nx_block,ny_block,ntrcr,ncat,max_blocks))
 
-   ! initialize to the default initial ice state
-   ! these could be set to values read from a file...
+!-----------------------------------------------------------------------
+! initialize to the default initial ice state
+! halo cells have to be filled manually at this stage
+! these arrays could be set to values read from a file...
+!-----------------------------------------------------------------------
 
       call ice_timer_start(timer_bound)
       call bound_state (aicen, trcrn, &
@@ -99,6 +113,110 @@
    vicen_rest(:,:,:,:) = vicen(:,:,:,:)
    vsnon_rest(:,:,:,:) = vsnon(:,:,:,:)
    trcrn_rest(:,:,:,:,:) = trcrn(:,:,1:ntrcr,:,:)
+
+   !$OMP PARALLEL DO PRIVATE(iblk,ilo,ihi,jlo,jhi,this_block, &
+   !$OMP                     i,j,n,nt,ibc,npad)
+   do iblk = 1, nblocks
+      this_block = get_block(blocks_ice(iblk),iblk)         
+         ilo = this_block%ilo
+         ihi = this_block%ihi
+         jlo = this_block%jlo
+         jhi = this_block%jhi
+
+      if (this_block%iblock == 1) then              ! west edge
+         if (trim(ew_boundary_type) /= 'cyclic') then
+            do n = 1, ncat
+            do j = 1, ny_block
+            do i = 1, ilo-1
+               aicen_rest(i,j,n,iblk) = aicen(ilo,j,n,iblk)
+               vicen_rest(i,j,n,iblk) = vicen(ilo,j,n,iblk)
+               vsnon_rest(i,j,n,iblk) = vsnon(ilo,j,n,iblk)
+               do nt = 1, ntrcr
+                  trcrn_rest(i,j,nt,n,iblk) = trcrn(ilo,j,nt,n,iblk)
+               enddo
+            enddo
+            enddo
+            enddo
+         endif
+
+      elseif (this_block%iblock == nblocks_x) then  ! east edge
+         if (trim(ew_boundary_type) /= 'cyclic') then
+            ! locate ghost cell column (avoid padding)
+            ibc = nx_block
+            do i = nx_block, 1, -1
+               npad = 0
+               if (this_block%i_glob(i) == 0) then
+                  do j = 1, ny_block
+                     npad = npad + this_block%j_glob(j)
+                  enddo
+               endif
+               if (npad /= 0) ibc = ibc - 1
+            enddo
+
+            do n = 1, ncat
+            do j = 1, ny_block
+            do i = ihi+1, ibc
+               aicen_rest(i,j,n,iblk) = aicen(ihi,j,n,iblk)
+               vicen_rest(i,j,n,iblk) = vicen(ihi,j,n,iblk)
+               vsnon_rest(i,j,n,iblk) = vsnon(ihi,j,n,iblk)
+               do nt = 1, ntrcr
+                  trcrn_rest(i,j,nt,n,iblk) = trcrn(ihi,j,nt,n,iblk)
+               enddo
+            enddo
+            enddo
+            enddo
+         endif
+      endif
+
+      if (this_block%jblock == 1) then              ! south edge
+         if (trim(ns_boundary_type) /= 'cyclic') then
+            do n = 1, ncat
+            do j = 1, jlo-1
+            do i = 1, nx_block
+               aicen_rest(i,j,n,iblk) = aicen(ilo,j,n,iblk)
+               vicen_rest(i,j,n,iblk) = vicen(ilo,j,n,iblk)
+               vsnon_rest(i,j,n,iblk) = vsnon(ilo,j,n,iblk)
+               do nt = 1, ntrcr
+                  trcrn_rest(i,j,nt,n,iblk) = trcrn(ilo,j,nt,n,iblk)
+               enddo
+            enddo
+            enddo
+            enddo
+         endif
+
+      elseif (this_block%jblock == nblocks_y) then  ! north edge
+         if (trim(ns_boundary_type) /= 'cyclic' .and. &
+             trim(ns_boundary_type) /= 'tripole' .and. &
+             trim(ns_boundary_type) /= 'tripoleT') then
+            ! locate ghost cell row (avoid padding)
+            ibc = ny_block
+            do j = ny_block, 1, -1
+               npad = 0
+               if (this_block%j_glob(j) == 0) then
+                  do i = 1, nx_block
+                     npad = npad + this_block%i_glob(i)
+                  enddo
+               endif
+               if (npad /= 0) ibc = ibc - 1
+            enddo
+
+            do n = 1, ncat
+            do j = jhi+1, ibc
+            do i = 1, nx_block
+               aicen_rest(i,j,n,iblk) = aicen(ihi,j,n,iblk)
+               vicen_rest(i,j,n,iblk) = vicen(ihi,j,n,iblk)
+               vsnon_rest(i,j,n,iblk) = vsnon(ihi,j,n,iblk)
+               do nt = 1, ntrcr
+                  trcrn_rest(i,j,nt,n,iblk) = trcrn(ihi,j,nt,n,iblk)
+               enddo
+            enddo
+            enddo
+            enddo
+         endif
+      endif
+
+   enddo ! iblk
+   !$OMP END PARALLEL DO
 
    if (my_task == master_task) &
       write (nu_diag,*) 'ice restoring timescale = ',trestore,' days' 
